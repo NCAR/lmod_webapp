1278ebf3ed9e3b7521f899b14a53634ae29c3311
Herok4Build <thomash.johnson261@gmail.com>
2019-06-21 11:18:02 -0600

    Made modifications to third implementation of the overall code.

--- dict_to_array_dict.py
+++ dict_to_array_dict.py
@@ -7,32 +7,32 @@
 #Reference: https://stackoverflow.com/questions/8995611/removing-multiple-keys-from-a-dictionary-safely
 #Reference: https://stackoverflow.com/questions/4406501/change-the-name-of-a-key-in-dictionary
 
-import copy
-def value_morph_dict(heading_container):
-    for key, content_array in heading_container.items():
-        array_dict_content = []
-        for element in content_array:
-            element = {"label": element, "value": str(element + "_value")}
-            array_dict_content.append(element)
-        heading_container[key] = array_dict_content
-    print("heading_container with array of dicts:")
-    for key, content_array in heading_container.items():
+import copy #Imports the copy module of Python for the purpose of making deep copies to maniulate dictionaries in this code
+def value_morph_dict(heading_container):# Function value_morph_dict function accepts a dictionary argument. Executes the remainder of the code in this Python file.
+    for key, content_array in heading_container.items():# Iterates through the dictionary
+        array_dict_content = [] #Initializes an outer list to wrap around the dictionaries that will be formed
+        for element in content_array:# For every indicidual piece of content, a dictionary eill be formed with keys and values
+            element = {"label": element, "value": str(element + "_value")}# Ditionary template for the dictionaries that will be formed
+            array_dict_content.append(element)# Appends the dictionary to the list arra_dict_content to keep dictionaries of all content contained and managed
+        heading_container[key] = array_dict_content #Now each dictionary will store all of dicitonaries contianing the content
+    print("heading_container with array of dicts:")# Print statement for debugging purposes
+    for key, content_array in heading_container.items():#For loop and print statement to track the initial organization of each piece of content
         print("\n",key, content_array)
-    full_compiler_container_complete = search_dict_children_from_target(heading_container)
-    return full_compiler_container_complete
+    full_compiler_container_complete = search_dict_children_from_target(heading_container) #Returns the complete list-dict construct to be stored in a variable for further usage
+    return full_compiler_container_complete#Returns the list-dict construct to reg_expression.py
 
-# search_dict_children_from_target fucntion accepts a string of content, an associated dictionary, a list of headings, a list of lists containing the content, and a heading cotaining the dictionary entry's children
-def search_dict_children_from_target(heading_container):
-    array_of_headings = list(heading_container.keys())
-    clone_of_array_of_headings = array_of_headings.copy()
-    heading_container_clone = copy.deepcopy(heading_container)
-    for key, value_array_suspect in heading_container_clone.items():
-        for value_suspect in value_array_suspect:
-            check_label_bool, parent_heading = check_label_in_headings(value_suspect,array_of_headings)
-            if check_label_bool == True and parent_heading != None:
-                location_of_value_suspect = value_array_suspect.index(value_suspect)
-                value_suspect = move_children(heading_container,array_of_headings, parent_heading, value_suspect, location_of_value_suspect)
-                print("\n\nPrinting the value_suspect here:")
+# search_dict_children_from_target fucntion has an argument of heading_container, will search for children of the given dictionary
+def search_dict_children_from_target(heading_container): # The function argument is already a heaing_container
+    array_of_headings = list(heading_container.keys()) # Creates a list of headings for tracking purposes
+    clone_of_array_of_headings = array_of_headings.copy()# Clones the array_of_headings
+    heading_container_clone = copy.deepcopy(heading_container)# Create a clone of the heading_container completely
+    for key, value_array_suspect in heading_container_clone.items():# For loop iterating through the directories and arrays of content in the heading_conteiner_clone dictionary
+        for value_suspect in value_array_suspect:# For loop iterating through the individual values of the array of content dictionary
+            check_label_bool, parent_heading = check_label_in_headings(value_suspect,array_of_headings)# Returns a boolean and a directory that would likely contain the children of a given content dictionary
+            if check_label_bool == True and parent_heading != None: #Checks to make sure theat the boolean is True and a value for parent_heading is provided, otherwise no children were found
+                location_of_value_suspect = value_array_suspect.index(value_suspect) #Tracks the content dictionary in the array that contains them
+                value_suspect = move_children(heading_container,array_of_headings, parent_heading, value_suspect, location_of_value_suspect)#Function that moves content dictionary array that has the children of said content dictionary, takes the overall structure, the headings that are remaining to utilize, the heading of the children, the content dictionary that has children, and the location of the content dictionary in the array of content dictionaries
+                print("\n\nPrinting the value_suspect here:") #Print statement that will be used for debugging purposes
                 print (value_suspect)
                 print("\n\nThe full heading_container is printed:")
                 print(heading_container)
@@ -87,8 +87,10 @@ def search_dict_descendants(heading_container,array_of_headings,item_with_possib
     print("Item with possible descendants:")
     print(item_with_possible_descendants)
     for content_container in item_with_possible_descendants:
-        check_sub_label_bool, parent_sub_heading = check_label_in_headings(content_container, array_of_headings)
-        if check_sub_label_bool == True and parent_sub_heading != None:
+        print("\nThis point prints content_contianer and array_of_headings:")
+        print(content_container,array_of_headings)
+        parent_sub_heading = check_lineage_label_in_headings(content_container, array_of_headings,item_suspect_parent)
+        if parent_sub_heading != None:
             location_of_content_container = item_with_possible_descendants.index(content_container)
             content_container = move_children(heading_container,array_of_headings, parent_sub_heading, content_container, location_of_content_container)
             print("The missing heading:")
@@ -105,3 +107,14 @@ def check_label_in_headings(target_item_dictionary, array_of_headings):# Checks 
         else:
             continue# Continues looping otherwise
     return False, None# If not a substring, this is returned
+
+def check_lineage_label_in_headings(target_item_content, array_of_headings, parent_of_target_item_content):
+    for target_heading in array_of_headings:
+        if target_item_content["label"] in target_heading:
+            if parent_of_target_item_content["label"] in target_heading:
+                located_target_heading = target_heading
+                print("\nPrint the located_target_heading: ",located_target_heading)
+                return located_target_heading
+        else:
+            continue
+        return None
