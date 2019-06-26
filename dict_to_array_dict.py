4ca0ff77887884286081cf52966faf7fb1161b7b
Herok4Build <thomash.johnson261@gmail.com>
2019-06-26 08:56:11 -0600

    Added another function to better track issues.

--- dict_to_array_dict.py
+++ dict_to_array_dict.py
@@ -42,13 +42,7 @@ def search_dict_children_from_target(heading_container): # The function argument
             if check_label_bool == True and parent_heading != None: #Checks to make sure theat the boolean is True and a value for parent_heading is provided, otherwise no children were found
                 location_of_value_suspect = value_array_suspect.index(value_suspect) #Tracks the content dictionary in the array that contains them
                 value_suspect = move_children(heading_container,array_of_headings, parent_heading, value_suspect, location_of_value_suspect)#Function that moves content dictionary array that has the children of said content dictionary, takes the overall structure, the headings that are remaining to utilize, the heading of the children, the content dictionary that has children, and the location of the content dictionary in the array of content dictionaries
-                print("\n\nPrinting the value_suspect here:") #Print statement that will be used for debugging purposes (tracking the current content dictionary being examined)
-                print (value_suspect)
-                print("\n\nThe full heading_container is printed:")#Print statement that will be used for debugging purposes (tracking the heading_container as it goes through changes)
-                print(heading_container)
-                print("The missing heading:")#Print statement that will be used for debugging purposes (tracking the directories that remain to be utilized for the purpose of tracking the children of dictionaries, which one has been removed)
-                missing_heading = heading_container.pop(parent_heading)#Removes a directory from the keys of the heading_container
-                print(missing_heading)
+                print_to_check_and_popping_heading(heading_container, value_suspect,parent_heading)
             else:
                 continue
         print("\nThe resulting heirarchical array-dict object is here:") #Further debugging (Track the rearrangement of list-dict construct)
@@ -59,6 +53,15 @@ def search_dict_children_from_target(heading_container): # The function argument
     print(heading_container_clone)
     full_compiler_container = elminate_superfluous_keys(heading_container_clone,array_of_headings, clone_of_array_of_headings) #Returns the heading container with extra directories eliminated to get rid of the clutter of keys that have already been rearranged in the list-dict hierarchy
     return full_compiler_container#Returns a cleaned up list-dict with no extra keys
+
+def print_to_check_and_popping_heading(heading_container,value_suspect_dictionary,parent_heading):
+    print("\n\nPrinting the value_suspect here:") #Print statement that will be used for debugging purposes (tracking the current content dictionary being examined)
+    print (value_suspect_dictionary)
+    print("\n\nThe full heading_container is printed:")#Print statement that will be used for debugging purposes (tracking the heading_container as it goes through changes)
+    print(heading_container)
+    print("The missing heading:")#Print statement that will be used for debugging purposes (tracking the directories that remain to be utilized for the purpose of tracking the children of dictionaries, which one has been removed)
+    missing_heading = heading_container.pop(parent_heading)#Removes a directory from the keys of the heading_container
+    print(missing_heading)
 
 #A function responsible for retitling the top level of the list-dict hierarchy
 def new_first_level_directories(target_partial_heading_container): #Accepts the dictionaries that will contain the top level directories of the list-dict construct as an argument
@@ -176,4 +179,4 @@ def top_levels_attainment(array_containing_headings):#The argument for the funct
             array_of_top_levels.append(heading) #The instances that satisfy the if statement are appended to the list that was previously intialized within this function
     print("An array of the top levels")# Prints the top levels of the headings/directories to check the eleements of the list
     print(array_of_top_levels)
-    return array_of_top_levels # Returns the list of the top levels of the list-dictionary construct 
+    return array_of_top_levels # Returns the list of the top levels of the list-dictionary construct
